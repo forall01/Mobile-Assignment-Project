cb3a22a558162dd53dd30d0eb1edc495e705c662
Wasit Poonjaroen <>
2018-05-19 23:40:12 +0700

    combine mobile list screen and mobile favorite screen into tab layout

--- app/src/main/java/forallstudio/mobilephone/main/MobileFragment.java
+++ app/src/main/java/forallstudio/mobilephone/main/MobileFragment.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import forallstudio.mobilephone.R;
+import forallstudio.mobilephone.allmobile.MobileListFragment;
 import forallstudio.mobilephone.databinding.FragmentMobileBinding;
+import forallstudio.mobilephone.favorite.MobileFavoriteFragment;
 
 public class MobileFragment extends Fragment {
 
@@ -31,12 +36,67 @@ public class MobileFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil
                 .inflate(inflater, R.layout.fragment_mobile, container, false);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(binding.toolbar);
+        initView();
         return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    private void initView() {
+        binding.tabLayout.setupWithViewPager(binding.pagerMobileInfo);
+        binding.pagerMobileInfo.setAdapter(new MobileInfoAdapter(getChildFragmentManager()));
+    }
+
+    @Nullable
+    public Fragment getFragmentFromViewPager(int viewId, int position) {
+        String tag = findTagFragmentFromViewPager(viewId, position);
+        return getChildFragmentManager().findFragmentByTag(tag);
+    }
+
+    public String findTagFragmentFromViewPager(int viewId, long index) {
+        return "android:switcher:" + viewId + ":" + index;
+    }
+
+    public class MobileInfoAdapter extends FragmentPagerAdapter {
+
+        public MobileInfoAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return MobileListFragment.newInstance();
+                case 1:
+                    return MobileFavoriteFragment.newInstance();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getString(R.string.mobile_list);
+                case 1:
+                    return getString(R.string.favorite_list);
+                default:
+                    return super.getPageTitle(position);
+            }
+        }
+
     }
 
 }
